a7acb2c65ddd39910e75e5586051b0bd15760ff7
sronga <sr899@georgetown.edu>
2018-06-06 15:07:43 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,9 +3,9 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, index)
   if board[index] == " "
     false
-  elseif board[index] == "X"
+  elsif board[index] == "X"
     true
-  elseif board[index] == "O"
+  elsif board[index] == "O"
    true
   end
 end
