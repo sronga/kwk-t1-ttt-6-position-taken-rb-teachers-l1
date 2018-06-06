cdd7ec4e7e77ac0868b0c4fa1fa2a86ff6812e2f
sronga <sr899@georgetown.edu>
2018-06-06 15:08:32 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,15 @@
-# code your #position_taken? method here!
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def position_taken?(board, index)
+  if board[index] == " "
+    false
+  elsif board[index] == "X"
+    true
+  elsif board[index] == "O"
+   true
+ elsif board[index] == nil
+   false
+ elsif board[index] == ""
+   false
+  end
+end
